19afe96d5274dded0a72a4a772259e1dc37f72be
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-21 12:01:15 +0200

    create indexRouter.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const router = require('./router/indexRouter')
 
 const PORT = process.env.PORT || 5000;
 const app = express()
