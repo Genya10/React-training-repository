b400c35d4430e595bc0afb917d57730de079df2f
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-22 13:20:37 +0200

    add create activationLink

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -14,13 +14,22 @@ class UserService {
     const hashPassword = await bcrypt.hash(password,3);
     const activationLink = uuid.v4();
     const user = await UserModel.create({email,password:hashPassword, activationLink});
-    await mailService.sendActivationMail(email,`${process.env.API_URL}/api/activate/activationLink`);
+    await mailService.sendActivationMail(email,`${process.env.API_URL}/api/activate/${activationLink}`);
 
     const userDto = new UserDto(user);
     const tokens = tokenService.generateTokens({...userDto});
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
     return {...tokens, user:userDto}
+  }
+  async activate(activationLink){
+    const user = await UserModel.findOne({activationLink})
+    if(!user){
+      throw new Error('Неккоретная ссылка активации')
+    }
+    user.isActivated = true;
+    await user.save();
+        
   }
 }
 
