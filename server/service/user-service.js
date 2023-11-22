f44b8c004c12a1cfec4935ee27c7575308ee9f84
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-22 14:00:03 +0200

    install express-validation

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,12 +4,13 @@ const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
+const ApiError = require('../expection/api-error');
 
 class UserService {
   async registration (email,password){
     const candidate = await UserModel.findOne({email})
     if(candidate){
-        throw new Error(`Пользователь с адресом ${email} уже существует`)
+        throw ApiError.BadRequest(`Пользователь с адресом ${email} уже существует`)
     }
     const hashPassword = await bcrypt.hash(password,3);
     const activationLink = uuid.v4();
@@ -25,11 +26,11 @@ class UserService {
   async activate(activationLink){
     const user = await UserModel.findOne({activationLink})
     if(!user){
-      throw new Error('Неккоретная ссылка активации')
+      throw ApiError.BadRequest('Неккоретная ссылка активации')
     }
     user.isActivated = true;
     await user.save();
-        
+
   }
 }
 
