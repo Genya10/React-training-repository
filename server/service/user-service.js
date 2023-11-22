b329be8292722a33b7e3476d8149dee132dc1f2b
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-22 10:12:01 +0200

    addition to user-service.js

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -2,7 +2,8 @@ const UserModel = require('../models/user-models');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
-const tokenService = require('./token-service')
+const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
   async registration (email,password){
@@ -11,10 +12,15 @@ class UserService {
         throw new Error(`Пользователь с адресом ${email} уже существует`)
     }
     const hashPassword = await bcrypt.hash(password,3);
-    const activateLink = uuid.v4();
+    const activationLink = uuid.v4();
     const user = await UserModel.create({email,password:hashPassword, activationLink});
-    await mailService.sendActivationMail(email,activateLink);
-    const tikens = tokenService.generateTokens();
+    await mailService.sendActivationMail(email,activationLink);
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {...tokens, user:userDto}
   }
 }
 
